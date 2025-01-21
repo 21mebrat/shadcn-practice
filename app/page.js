237fe34994943af928f882ffbb28ae966d016d77
21mebrat <maytotmat@gmail.com>
2025-01-21 12:42:59 +0300

    setup the darkmode

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='text-4xl text-blue-700 text-center'>
-      This is the home page on is it
+      This is the hhahhhome page on is it
       so one is hat 
     </div>
   )
