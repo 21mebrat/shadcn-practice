bce75fc6d8265bd9c468a2b5cee2b4605dbc0875
21mebrat <maytotmat@gmail.com>
2025-01-21 11:14:08 +0300

    scibd

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='text-4xl text-blue-700 text-center'>
-      This is the home page
+      This is the home page on is it
 
     </div>
   )
